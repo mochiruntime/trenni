b07dbcfe801477a8ebdc86e9db40c0752f7dee42
Alexandre Chau <14943421+mochiruntime@users.noreply.github.com>
2026-04-06 19:12:43 +0000

    chore: initial commit

---
+++ test/unit_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Unit Tests', () {
+    test('Example unit test', () {
+      const int result = 2 + 2;
+      expect(result, 4);
+    });
+  });
+}
